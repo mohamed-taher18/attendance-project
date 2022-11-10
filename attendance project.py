69ffe2c807e15cf9572e139b76061bc822f2c4b3
mohamed-taher18 <83633589+mohamed-taher18@users.noreply.github.com>
2022-11-10 17:07:32 +0200

    Add files via upload

---
+++ attendance project.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+path = "G:/project/images"
+images = []
+classNames = []
+mylist = os.listdir(path)
+print(mylist)
+for cl in mylist:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+def markattendance(name):
+    with open('G:/project/attendance.csv','r+') as f:
+        myDataList = f.readlines()
+        nameList = []
+        for line in myDataList:
+            entry = line.split(",")
+            nameList.append(entry[0])
+        if name not in nameList:
+            now =datetime.now()
+            dtString = now.strftime("%H:%M:%S")
+            f.writelines(f'\n{name},{dtString}')
+
+encodListKnown = findEncodings(images)
+print("encoding complete")
+cap = cv2.VideoCapture(0)
+while True:
+    succes, img =cap.read()
+    imgS = cv2.resize(img,(0,0),None,0.25,0.25)
+    imgS = cv2.cvtColor(imgS , cv2.COLOR_BGR2RGB)
+    facesCurrFrame = face_recognition.face_locations(imgS)
+    encodeCurrFrame = face_recognition.face_encodings(imgS,facesCurrFrame)
+    for encodeFace,faceLoc in zip(encodeCurrFrame,facesCurrFrame):
+        matches = face_recognition.compare_faces(encodListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodListKnown,encodeFace)
+        #print(faceDis)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            #print(name)
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_DUPLEX,1,(255,255,255),2 )
+            markattendance(name)
+
+    cv2.imshow("webcam",img)
+    cv2.waitKey(1)
+
